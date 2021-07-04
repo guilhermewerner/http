b8b36dd68dbd472e30f97bcb484e8a6c927c7f9c
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-07-04 10:16:04 -0300

    Add Bootstrap Scripts

---
+++ Bootstrap.ps1
@@ -0,0 +1,19 @@
+#!/usr/bin/env pwsh
+
+# Windows
+
+if ($IsWindows)
+{
+}
+
+# Mac
+
+elseif ($IsMacOS)
+{
+}
+
+# Linux
+
+elseif ($IsLinux)
+{
+}
---
+++ Bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Linux
+
+if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
+then
+
+# Mac
+
+elif [ "$(uname)" = "Darwin" ]
+then
+
+fi
