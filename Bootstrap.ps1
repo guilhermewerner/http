2b57aaa9e4a8e978a1707d3de199481633f1a1a5
GuilhermeWerner <26710260+GuilhermeWerner@users.noreply.github.com>
2021-09-03 10:56:17 -0300

    Update bootstrap scripts

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,19 +1,1 @@
 #!/usr/bin/env pwsh
-
-# Windows
-
-if ($IsWindows)
-{
-}
-
-# Mac
-
-elseif ($IsMacOS)
-{
-}
-
-# Linux
-
-elseif ($IsLinux)
-{
-}
--- Bootstrap.sh
+++ Bootstrap.sh
@@ -1,13 +1,1 @@
 #!/usr/bin/env sh
-
-# Linux
-
-if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
-then
-
-# Mac
-
-elif [ "$(uname)" = "Darwin" ]
-then
-
-fi
